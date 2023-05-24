cab0dc5f88f30869b38b0b9d891757fb2f01ed4c
idko <ivancito72011@gmail.com>
2023-05-24 11:54:10 -0400

    posibles casos especiales que no estoy seguro si poner

--- frontend/js/cards.js
+++ frontend/js/cards.js
@@ -87,7 +87,15 @@ function validCard(card, currentCard)
 	sameColor = currentCardProps.color === cardProps.color;
 	sameValue = currentCardProps.value === cardProps.value;
 
+	//Si alguna de las cartas no tiene color (o color blanco)
 	if(currentCardProps.color === null || cardProps.color === null) sameColor = true;
+
+	/*
+	//Casos especiales
+
+	//Si la carta actual es un +2 o +6 y la carta elegida es para bloquear
+	if(['+2', '+6'].includes(currentCardProps.value) && cardProps.value === 'BLOCK') return true;
+	*/
 
 	return sameColor || sameValue;
 }
